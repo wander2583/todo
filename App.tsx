e96f6053137ffc807bd6525241e294eb09a48086
Wanderlei <wander@pop-os.localdomain>
2023-04-04 07:42:44 -0300

    feat: added app-container theme to project.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppContainer from './src/components/app-container';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer>
+      <View style={styles.container}>
+        <Text>Ola, Mundo!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </AppContainer>
   );
 }
 

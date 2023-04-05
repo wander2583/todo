fa7b4badae7829a33174327473c8db7844363959
Wanderlei <wander@pop-os.localdomain>
2023-04-04 23:14:58 -0300

    feat: trying to place the check button.

---
+++ src/components/animated-checkbox.tsx
@@ -0,0 +1,16 @@
+import Svg, { Path } from 'react-native-svg'
+
+
+const MARGIN = 10
+const vWidth = 64 + MARGIN
+const vHeight = 64 + MARGIN
+
+const AnimatedCheckbox = () => {
+  return (
+    <Svg viewBox={[-MARGIN, -MARGIN, vWidth + MARGIN, vHeight + MARGIN].join(' ')}>
+      <Path d="M126 2H57C26.6243 2 2 26.6243 2 57V103C2 133.376 26.6243 158 57 158H126C156.376 158 181 133.376 181 103V57C181 26.6243 156.376 2 126 2ZM57 0C25.5198 0 0 25.5198 0 57V103C0 134.48 25.5198 160 57 160H126C157.48 160 183 134.48 183 103V57C183 25.5198 157.48 0 126 0H57Z" fill="black"/>
+      <Path d="M38 62C68 62 88 80 88 122C100 91 133 17 213 29" stroke="black" stroke-width="2"/>
+    </Svg>
+  )
+}
+export default AnimatedCheckbox
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -7,11 +7,15 @@ import {
   useColorModeValue, 
   VStack} from 'native-base'
 import ThemeToogle from '../components/theme-toggle'
+import AnimatedCheckbox from '../components/animated-checkbox'
 
 export default function MainScreen() {
   return (
     <Center _dark={{bg: 'blueGray.900'}} _light={{bg: 'blueGray.50'}} px={4} flex={1} >
       <VStack space={5} alignItems="center" >
+        <Box w="100px" h="100px">
+          <AnimatedCheckbox />
+        </Box>
         <Box p={10} bg={useColorModeValue('red.500', 'yelow.500')}>
           <Text>Hello</Text>
         </Box>
